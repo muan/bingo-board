323888653b5335d762679468ce9686884f49a420
Mu-An Chiou <me@muanchiou.com>
2015-11-07 13:41:50 +0000

    Add tests to prevent numbered files being added to this repository

---
+++ test.js
@@ -0,0 +1,11 @@
+var fs = require('fs')
+var test = require('tape')
+
+fs.readdir('./_data/numbers', function (err, list) {
+	if (err) return
+
+	test('No extra files in _data/numbers', function (t) {
+		t.equal(list.length, 1, '.gitkeep should be the only file in _data/numbers')
+		t.end()
+	})
+})
